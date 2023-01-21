811c1b24a672b32c2fc4b6495d70b418740dd3a5
azez0 <nanquery@gmail.com>
2023-01-21 11:46:24 +0100

    add logging options

--- print_logs.py
+++ print_logs.py
@@ -1,16 +1,22 @@
 import logging
 
 default_log_format = "%(asctime)s - %(levelname)s - %(message)s"
-level = input("Enter logging level (DEBUG, INFO): ").upper()
+level = input("Enter logging level [DEBUG, INFO, WARNING, CRITICAL, ERROR]: ").upper()
 
 
 def set_logging(log_format):
     logging_level = getattr(logging, level, None)
     logging.basicConfig(level=logging_level, format=log_format)
     if level == "DEBUG":
-        logging.debug("Logging is set to level: %s and format: %s", level, log_format)
+        logging.debug('Protocol problem: %s', 'connection reset')
     if level == "INFO":
-        logging.info("Logging is set to level: %s and format: %s", level, log_format)
+        logging.info('Protocol problem: %s', 'connection reset')
+    if level == "WARNING":
+        logging.warning('Protocol problem: %s', 'connection reset')
+    if level == "ERROR":
+        logging.error('Protocol problem: %s', 'connection reset')
+    if level == "CRITICAL":
+        logging.critical('Protocol problem: %s', 'connection reset')
 
 
 set_logging(default_log_format)
